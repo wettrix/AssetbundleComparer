c0dac1c41bfec1418b853cfee3b51164cb60222b
wettrix <gustav.knutsson@wettrix.net>
2018-04-21 18:04:20 +0900

    Constant Class

---
+++ Assets/GKStudio/Editor/Scripts/Constants.cs
@@ -0,0 +1,16 @@
+﻿namespace GKStudio
+{
+	/**
+	 * Constants.cs
+	 * 
+	 * ?
+	 * ------------------------------------
+	 * ?
+	 * 
+	 * 2018-04-21	GKStudio 	Gustav Knutsson
+	 */
+	public class Constants
+	{
+		
+	}
+}
